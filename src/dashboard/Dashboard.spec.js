c81eb765df543852f52eedde898062073af95b66
theJOHNwatt <j.watt10@gmail.com>
2019-10-15 13:08:04 -0700

    tests created

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,10 @@
 // Test away
+
+import React from 'react';
+import {render} from '@testing-library/react';
+import Dashboard from './Dashboard';
+
+test('Renders Properly', () => {
+    render(<Dashboard/>)
+})
+
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,16 @@
 // Test away!
+
+import React from 'react';
+import {render, fireEvent} from '@testing-library/react';
+import Display from './Display';
+
+test('Gate is locked', () => {
+    const setLockedMock = jest.fn();
+    const setClosedMock = jest.fn();
+    const { getByText } = render(<Display locked={setLockedMock} closed={setClosedMock}/>)
+    const lockedGate = getByText(/locked/i);
+    const closedGate = getByText(/closed/i);
+})
+
+
+
